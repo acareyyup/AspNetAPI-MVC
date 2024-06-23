c96d92642a240f29f385f50f24d722f95d5bca1b
Eyyüp Acar <100216722+acareyyup@users.noreply.github.com>
2024-06-23 17:25:50 +0300

    GetProductsWithCategory

--- NLayer.API/Controllers/ProductsController.cs
+++ NLayer.API/Controllers/ProductsController.cs
@@ -10,11 +10,13 @@ namespace NLayer.API.Controllers
     {
         private readonly IMapper _mapper;
         private readonly IService<Product> _service;
+        private readonly IProductService _productService;
 
-        public ProductsController(IMapper mapper, IService<Product> service)
+        public ProductsController(IMapper mapper, IService<Product> service, IProductService productService)
         {
             _mapper = mapper;
             _service = service;
+            _productService = productService;
         }
 
 
@@ -57,5 +59,14 @@ namespace NLayer.API.Controllers
             await _service.RemoveAsync(product);
             return CreateActionResult(CustomResponseDto<NoContentDto>.Success(204));
         }
+
+        // GET api/products/GetProductsWithCategory
+        [HttpGet("[action]")]
+        public async Task<IActionResult> GetProductsWithCategory()
+        {
+
+            return CreateActionResult(await _productService.GetProductsWitCategory());
+        }
+
     }
 }
