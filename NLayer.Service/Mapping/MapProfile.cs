600c2f3417872f1ef8b5d8f6ccd97806a3e67dbe
Eyyüp Acar <100216722+acareyyup@users.noreply.github.com>
2024-06-23 18:01:33 +0300

    Mapping

--- NLayer.Service/Mapping/MapProfile.cs
+++ NLayer.Service/Mapping/MapProfile.cs
@@ -11,8 +11,10 @@ namespace NLayer.Service.Mapping
             CreateMap<Product, ProductDto>().ReverseMap();
             CreateMap<Category, CategoryDto>().ReverseMap();
             CreateMap<ProductFeature, ProductFeatureDto>().ReverseMap();
+            CreateMap<BaseEntity, BaseDto>().ReverseMap();
             CreateMap<ProductUpdateDto, Product>();
             CreateMap<Product, ProductWithCategoryDto>();
+            CreateMap<Category, CategoryWithProductsDto>();
         }
     }
 }
