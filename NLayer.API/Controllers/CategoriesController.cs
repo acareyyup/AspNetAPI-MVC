97c7dbceb7f59063130da207773aae81861075fa
Eyyüp Acar <100216722+acareyyup@users.noreply.github.com>
2024-06-24 15:51:56 +0300

    CategoryApiService Methods

--- NLayer.API/Controllers/CategoriesController.cs
+++ NLayer.API/Controllers/CategoriesController.cs
@@ -1,4 +1,6 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using AutoMapper;
+using Microsoft.AspNetCore.Mvc;
+using NLayer.Core.DTOs;
 using NLayer.Core.Services;
 
 namespace NLayer.API.Controllers
@@ -6,17 +8,34 @@ namespace NLayer.API.Controllers
     public class CategoriesController : CustomBaseController
     {
         private readonly ICategoryService _categoryService;
-
-        public CategoriesController(ICategoryService categoryService)
+        private readonly IMapper _mapper;
+        public CategoriesController(ICategoryService categoryService, IMapper mapper)
         {
             _categoryService = categoryService;
+            _mapper = mapper;
         }
 
-        [HttpGet("[action]/{categoryId}")]
-        public async Task<IActionResult> GetSingleCategoryByIdWithProductAsync(int categoryId)
+
+        [HttpGet]
+        public async Task<IActionResult> GetAll()
         {
-            return CreateActionResult(await _categoryService.GetSingleCategoryByIdWithProductAsync(categoryId));
+
+            var categories = await _categoryService.GetAllAsync();
+
+            var categoriesDto = _mapper.Map<List<CategoryDto>>(categories.ToList());
+
+            return CreateActionResult(CustomResponseDto<List<CategoryDto>>.Success(200, categoriesDto));
+
         }
 
+
+        // api/categories/GetSingleCategoryByIdWithProducts/2
+        [HttpGet("[action]/{categoryId}")]
+        public async Task<IActionResult> GetSingleCategoryByIdWithProducts(int categoryId)
+        {
+
+            return CreateActionResult(await _categoryService.GetSingleCategoryByIdWithProductAsync(categoryId));
+
+        }
     }
 }
