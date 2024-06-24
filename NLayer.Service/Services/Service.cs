41a8aadcce3632d8571916023e98d656c4eb863b
Eyyüp Acar <100216722+acareyyup@users.noreply.github.com>
2024-06-24 11:16:16 +0300

    Service katmanında hata fırlatmak

--- NLayer.Service/Services/Service.cs
+++ NLayer.Service/Services/Service.cs
@@ -2,6 +2,7 @@
 using NLayer.Core.IUnitOfWorks;
 using NLayer.Core.Repositories;
 using NLayer.Core.Services;
+using NLayer.Service.Exceptions;
 using System.Linq.Expressions;
 
 namespace NLayer.Service.Services
@@ -44,7 +45,13 @@ namespace NLayer.Service.Services
 
         public async Task<T> GetByIdAsync(int id)
         {
-            return await _repository.GetByIdAsync(id);
+            var hasProduct = await _repository.GetByIdAsync(id);
+
+            if (hasProduct == null)
+            {
+                throw new NotFoundExcepiton($"{typeof(T).Name}({id}) not found");
+            }
+            return hasProduct;
         }
 
         public async Task RemoveAsync(T entity)
